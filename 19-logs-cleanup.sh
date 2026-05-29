d63a51b95ed3ea87c719529f1ed30ccc153db3ac
joindevopscloud <info@joindevops.com>
2026-05-29 07:29:43 +0530

    oil and karam added

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -3,14 +3,14 @@
 SOURCE_DIR=$1
 DAYS=${2:-14} # default 14 days
 
-if [ -z $SOURCE_DIR ]; then
+if [ -z "$SOURCE_DIR" ]; then
     echo "ERROR:: Missing parameters"
     echo "USAGE:: $0 <source-dir> [days(optional default to 14)]"
     exit 1
 fi
 
-if [ ! -d $SOURCE_DIR ]; then
-    echo "ERROR: Directory: $SOURCE_DIR does not exist"
+if [ ! -d "$SOURCE_DIR" ]; then
+    echo "ERROR:: Directory: $SOURCE_DIR does not exist"
     exit 1
 fi
 
