30806dac75e4830bb97d63e947e052992cc43351
joindevopscloud <info@joindevops.com>
2026-05-29 08:31:45 +0530

    oil and karam added

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=10
+SERVER_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read -r line
 do
@@ -14,3 +15,5 @@ do
 done <<< "$DISK_USAGE"
 
 echo -e "$MESSAGE"
+
+sh mail.sh "DevOps Team" "High Disk usage" "$SERVER_IP" "$MESSAGE" "info@joindevops.com" "High Disk Usage Alert"
